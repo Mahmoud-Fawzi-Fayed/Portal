eaea9ad402faa433137738541cfe7e04b01bc1f6
Mahmoud-Fawzi-Fayed <Mahmoud-Fawzi-Fayed>
2023-11-13 19:07:22 +0200

    collages_home_page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  
   highlights = [
     {
       image: 'assets/images/placeholder.jpg',
@@ -76,33 +77,26 @@ export class HomeComponent {
   ];
 
   cardRows = [
-    [
-      { image: 'assets/images/medicine.jpg', link: 'https://faculty1.example.com', name: 'كلية الطب' },
-      { image: 'assets/images/ai.png', link: 'https://faculty1.example.com', name: 'كلية الذكاء الاصطناعي' },
-      { image: 'assets/images/computers.jpg', link: 'https://faculty1.example.com', name: 'كلية الحاسبات والمعلومات' },
-      { image: 'assets/images/education.png', link: 'https://faculty1.example.com', name: 'كلية التربية' },
-      { image: 'assets/images/engineering.png', link: 'https://faculty1.example.com', name: 'كلية الهندسة' },
-    ],
-    [
-      { image: 'assets/images/literature.jpg', link: 'https://faculty1.example.com', name: 'كلية الآداب' },
-      { image: 'assets/images/nursing.jpg', link: 'https://faculty1.example.com', name: 'كلية التمريض' },
-      { image: 'assets/images/pharmacy.jpg', link: 'https://faculty1.example.com', name: 'كلية الصيدلة' },
-      { image: 'assets/images/vet.png', link: 'https://faculty1.example.com', name: 'كلية الطب البيطري' },
-      { image: 'assets/images/commerce.png', link: 'https://faculty1.example.com', name: 'كلية التجارة' },
-    ],
-    [
-      { image: 'assets/images/education_kids.jpg', link: 'https://faculty1.example.com', name: 'كلية التربية للطفولة المبكرة' },
-      { image: 'assets/images/laws.jpg', link: 'https://faculty1.example.com', name: 'كليةالحقوق' },
-      { image: 'assets/images/mass.jpg', link: 'https://faculty1.example.com', name: 'كلية الإعلام' },
-      { image: 'assets/images/physical_education.png', link: 'https://faculty1.example.com', name: 'كلية التربية الرياضية' },
-      { image: 'assets/images/agriculture.jpg', link: 'https://faculty1.example.com', name: 'كلية الزراعة' },
-    ],
-    [
-      { image: 'assets/images/dentistry.png', link: 'https://faculty1.example.com', name: 'كلية طب الأسنان' },
-      { image: 'assets/images/menof.png', link: 'https://faculty1.example.com', name: 'كلية الهندسة الإلكترونية' },
-      { image: 'assets/images/economics.png', link: 'https://faculty1.example.com', name: 'كلية الاقتصاد المنزلي' },
-      { image: 'assets/images/applied_health.jpg', link: 'https://faculty1.example.com', name: 'كلية تكنولوجيا العلوم التطبيقية' },
-      { image: 'assets/images/science.jpg', link: 'https://faculty1.example.com', name: 'كلية العلوم' },
-    ],
+    [{ image: 'assets/images/medicine.jpg', link: '#', name: 'كلية الطب' }],
+    [{ image: 'assets/images/ai.png', link: '#', name: 'كلية الذكاء الاصطناعي' }],
+    [{ image: 'assets/images/computers.jpg', link: '#', name: 'كلية الحاسبات والمعلومات' }],
+    [{ image: 'assets/images/education.png', link: '#', name: 'كلية التربية' }],
+    [{ image: 'assets/images/engineering.png', link: '#', name: 'كلية الهندسة' }],
+    [{ image: 'assets/images/literature.jpg', link: '#', name: 'كلية الآداب' }],
+    [{ image: 'assets/images/nursing.jpg', link: '#', name: 'كلية التمريض' }],
+    [{ image: 'assets/images/pharmacy.jpg', link: '#', name: 'كلية الصيدلة' }],
+    [{ image: 'assets/images/vet.png', link: '#', name: 'كلية الطب البيطري' }],
+    [{ image: 'assets/images/commerce.png', link: '#', name: 'كلية التجارة' }],
+    [{ image: 'assets/images/education_kids.jpg', link: '#', name: 'كلية التربية للطفولة المبكرة' }],
+    [{ image: 'assets/images/laws.jpg', link: '#', name: 'كليةالحقوق' }],
+    [{ image: 'assets/images/mass.jpg', link: '#', name: 'كلية الإعلام' }],
+    [{ image: 'assets/images/physical_education.png', link: '#', name: 'كلية التربية الرياضية' }],
+    [{ image: 'assets/images/agriculture.jpg', link: '#', name: 'كلية الزراعة' }],
+    [{ image: 'assets/images/dentistry.png', link: '#', name: 'كلية طب الأسنان' }],
+    [{ image: 'assets/images/menof.png', link: '#', name: 'كلية الهندسة الإلكترونية' }],
+    [{ image: 'assets/images/economics.png', link: '#', name: 'كلية الاقتصاد المنزلي' }],
+    [{ image: 'assets/images/applied_health.jpg', link: '#', name: 'كلية تكنولوجيا العلوم التطبيقية' }],
+    [{ image: 'assets/images/science.jpg', link: '#', name: 'كلية العلوم' }],
   ];
+
 }
